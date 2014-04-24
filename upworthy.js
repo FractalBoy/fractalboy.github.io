1884cb76ed684f6c6c6178dce3be138c31226edb
Marc Reisner <reisner.marc@gmail.com>
2014-04-24 03:38:53 -0400

    upworthy.js

--- upworthy.js
+++ upworthy.js
@@ -1,4 +1,21 @@
-Array.prototype.slice.call(document.getElementsBiyClassName('_4-u2 mbm _5jmm _5pat _5v3q _5sq8 _5x16')).forEach(function(div) {
+// ==UserScript==
+// @name        Remove Upworthy links from Facebook
+// @namespace   fractalboy.github.io
+// @description Removes Upworthy links from Facebook so it doesn't annoy you
+// @include     https://www.facebook.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+// ==UserScript==
+// @name        Remove Upworthy links from Facebook
+// @namespace   fractalboy.github.io
+// @description Removes Upworthy links from Facebook so it doesn't annoy you
+// @include     https://www.facebook.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+window.setInterval(function() {
+Array.prototype.slice.call(document.getElementsByClassName('_4-u2 mbm _5jmm _5pat _5v3q _5sq8 _5x16')).forEach(function(div) {
     var found = false;
     Array.prototype.slice.call(div.getElementsByClassName('_6lz _6mb ellipsis')).forEach(function(div1) {
         if(div1.textContent.indexOf('Upworthy') > -1) {
@@ -9,3 +26,4 @@ Array.prototype.slice.call(document.getElementsBiyClassName('_4-u2 mbm _5jmm _5p
         div.parentElement.removeChild(div);
     }
 });
+}, 500);
