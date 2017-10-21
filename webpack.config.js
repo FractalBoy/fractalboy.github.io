9352e021cdb39333365d7149d176b20e6e4a86a6
Marc Reisner <reisner.marc@gmail.com>
2017-10-21 17:07:42 -0500

    fix files that should and should not be committed

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default']
+        })
+    ]
+};
+
+
