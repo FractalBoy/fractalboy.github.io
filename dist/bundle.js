1654fc79f3467c79c5d21886f0745e2001255900
Marc Reisner <reisner.marc@gmail.com>
2017-10-21 17:56:30 -0500

    add google drive stuff

--- dist/bundle.js
+++ dist/bundle.js
@@ -84,11 +84,76 @@ var _fountain2 = _interopRequireDefault(_fountain);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-(0, _jquery2.default)(function () {
-    (0, _jquery2.default)("#convert").click(function () {
-        alert("Clicked!");
+var API_KEY = "AIzaSyBXCaeaIaRz9YC7n52VYqCFNAmB4jUmyaI";
+var CLIENT_ID = "269066866991-qska5al6semmmli31jdm6k35liblc91o.apps.googleusercontent.com";
+
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+var SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+
+var authorizeButton = document.getElementById("authorize-button");
+var signoutButton = document.getElementById("signout-button");
+
+var handleClientLoad = function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+};
+
+var initClient = function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    }).then(function () {
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
     });
-});
+};
+
+var updateSigninStatus = function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+        listFiles();
+    } else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+    }
+};
+
+var handleAuthClick = function handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+};
+
+var handleSignoutClick = function handleSignoutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+};
+
+var appendPre = function appendPre(message) {
+    var pre = document.getElementById('content');
+    var textContent = document.createTextNode(message + '\n');
+    pre.appendChild(textContent);
+};
+
+var listFiles = function listFiles() {
+    gapi.client.drive.files.list({
+        'pageSize': 10,
+        'fields': "nextPageToken, files(id, name)"
+    }).then(function (response) {
+        appendPre('Files:');
+        var files = response.result.files;
+        if (files && files.length > 0) {
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                appendPre(file.name + ' (' + file.id + ')');
+            }
+        } else {
+            appendPre('No files found.');
+        }
+    });
+};
 
 /***/ }),
 /* 1 */
