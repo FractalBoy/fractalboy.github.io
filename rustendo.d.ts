8545f82476a0b251052983d4c7c5abc8132281cc
Marc Reisner <reisner.marc@gmail.com>
2021-04-24 02:11:36 -0500

    Use webpacked files

--- rustendo.d.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-/**
-*/
-export function startup(): void;
-/**
-* @param {Uint8Array} byte_array 
-*/
-export function render(byte_array: Uint8Array): void;
